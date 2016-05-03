ff660a0d00ac6334f550b3bafd8e611e0d95e7ff
Ben Silva <berna.loyola@gmail.com>
2016-05-03 14:31:02 -0700

    implemented basic add contact form functionalities

--- app/actions.rb
+++ app/actions.rb
@@ -2,10 +2,18 @@ get '/' do
   erb :index
 end
 
-get '/contacts' do
+get '/contacts/?' do
 	if params[:query]
 		Contact.search(params[:query]).to_json
 	else
 		Contact.all.to_json
+	end
+end
+
+post '/contacts/?' do
+	if params[:first_name] && params[:last_name] && params[:email]
+		Contact.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
+	else
+		status 400
 	end
 end
